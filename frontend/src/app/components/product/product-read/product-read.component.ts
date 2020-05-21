e09108a8212e46292d7676a3445540cf3f2d8790
Diogo Silva <diogosilva@prodata.inf.br>
2020-05-21 14:08:39 -0300

    Commit projeto completo

---
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -0,0 +1,22 @@
+import { ProductService } from './../../../components/product/product.service';
+import { Product } from "src/app/components/product/product.model";
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-product-read",
+  templateUrl: "./product-read.component.html",
+  styleUrls: ["./product-read.component.css"],
+})
+export class ProductReadComponent implements OnInit {
+  products: Product[];
+  displayedColumns = ['id', 'name', 'price', 'action'];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.productService.read().subscribe(products => {
+      this.products = products;
+      console.log(products)
+    });
+  }
+}
